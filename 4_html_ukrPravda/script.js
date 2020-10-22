289677fcc058ba4d631220948e39d18b0c230435
Yurii Chornii <65965605+lira-lux@users.noreply.github.com>
2020-10-22 21:30:02 +0300

    done

--- 4_html_ukrPravda/script.js
+++ 4_html_ukrPravda/script.js
@@ -1,1 +1,10 @@
 // alert('ok');
+const btn = document.getElementById('btn-oll-sections');
+btn.onclick = () => {
+    const background = document.getElementById('logo-wrapper');
+    background.classList.toggle('logo-dropdown-active');
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('dropdown-visibility');
+    const icon = document.getElementById('btn-icon-open-close-dropdown');
+    icon.classList.toggle('fa-times');
+}
